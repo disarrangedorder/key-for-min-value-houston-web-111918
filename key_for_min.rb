26081b7e5792e39acd00adef941c3fadefe2d098
Claire Nguyen <ms.clairenguyen@gmail.com>
2018-11-18 20:43:27 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,10 @@ def key_for_min_value(name_hash)
   smallest_hash_value = nil
   smallest_hash_key = nil
 
-  if name_hash == {} return smallest_hash_key end
-  else
+  if name_hash == {} return nil end
+ else
     name_hash.collect do |key, value|
-      if value < smallest_hash_value
+      if smallest_hash_value == nil || value < smallest_hash_value
         smallest_hash_key = key
         smallest_hash_value = value
       end
